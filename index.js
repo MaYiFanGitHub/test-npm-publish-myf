cd6ca00cf12c7df68029f0d264c554c7bb5a9d93
mayifan02 <mayifan02@baidu.com>
2020-11-17 20:00:43 +0800

    1

---
+++ index.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+const exec = require('child_process').exec;
+const { exit } = require('process');
+const chalk = require('chalk');
+
+/* 命令打印输出 */
+const __print = work => {
+    work.stdout.on('data', d => {
+        console.log(chalk.green('[INFO] '), d);
+    });
+    work.stderr.on('data', d => {
+        console.log(chalk.red('[ERR] ' + d));
+        exit(1);
+    });
+};
+
+/* 判断登陆 */
+const isLogin = (success, error) => {
+    let cmd = '(echo "mayifan" && sleep 1 && echo "qq9320996688" && sleep 1 && echo "83964472@qq.com") | npm login';
+    __print(
+        exec(cmd, (err, stdout, stderr) => {
+            if (!err) {
+                success();
+            } else {
+                error();
+            }
+        })
+    );
+};
+
+/* git pull */
+const gitPull = (success, error) => {
+    console.log(chalk.green('√'), '同步最新代码...')
+    let cmd = 'git pull --rebase';
+    __print(
+        exec(cmd, (err, stdout, stderr) => {
+            if (!err) {
+                success();
+            } else {
+                error();
+            }
+        })
+    );
+}
+gitPull(() => {  }, () => {})
+// isLogin(() => {console.log(1);}, () => {console.log(2);});
+
+
+// let cmd = '(echo "mayifan" && sleep 1 && echo "qq9320996688" && sleep 1 && echo "83964472@qq.com") | npm login';
+// let cmd = 'npm whoami';
+
+// __print(ls);
+
+
---
+++ test.sh
@@ -0,0 +1,13 @@
+# #!/usr/bin/expect
+# spawn npm login
+# expect "Username:"
+# send "mayifan\n"
+# expect "Password:"
+# send "9320996688\n"
+# expect "Email: (this IS public)"
+# send "83964472@qq.com\n"
+# #interact
+# expect off
+
+#!/bin/bash  
+(echo "mayifan" && sleep 1 && echo "qq9320996688" && sleep 1 && echo "83964472@qq.com") | npm login
