1571dcf789cf63c83a78ed715646593f834fbf48
mayifan02 <mayifan02@baidu.com>
2020-11-18 13:58:15 +0800

    icafeId: 123123, 修改信息：0000

--- publish_version.sh
+++ publish_version.sh
@@ -85,7 +85,7 @@ function gather_info() {
 # 提交代码
 function commit_code() {
     git add .
-    git commit -m "icafeId: $icafe_id, $commet_info"
+    git commit -m "icafeId: $icafe_id, 修改信息：$commet_info"
     if [ $? -eq 1 ]; then
         print "---提交代码失败...----" "[31m"
         exit 1
